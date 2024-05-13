073d23648c77845be8ebae00569f03b8f8f2e325
FMarin2401 <144305495+FMarin2401@users.noreply.github.com>
2024-05-12 20:53:05 -0600

    Mensajes

--- snake.py
+++ snake.py
@@ -9,18 +9,12 @@ screen = pygame.display.set_mode(tamano)  # Crea una ventana con el tamaño espe
 class Juego():
     def __init__(self):
         self.tablero_fondo = Fondo()
-        self.snake_cuerpo = [Cuerpo(100, 100)]  # Inicializa la serpiente con un solo segmento en la posición (100, 100)
+        self.snake_cuerpo = [Serpiente(100, 100)]  # Inicializa la serpiente con un solo segmento en la posición (100, 100)
         self.direccion = "RIGHT"  # Inicializa la dirección de la serpiente
         self.velocidad = 0.5 #Velocidad de la serpiente
         self.manzana = Manzana()  # Agregar la manzana al juego
 
-    def movimiento_teclado(self, event):
-        """
-        Maneja los eventos de teclado para controlar la dirección de la serpiente.
-
-        Args:
-            event: El evento de teclado.
-        """
+    def movimiento_teclado(self, event): #Controles de la serpíente
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP or event.key == pygame.K_w:
                 self.direccion = "UP"
@@ -31,28 +25,16 @@ class Juego():
             elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                 self.direccion = "RIGHT"
 
-    def dibuja(self, screen):
-        """
-        Dibuja el tablero y el cuerpo en la pantalla.
-
-        Args:
-            screen (Surface): La superficie de la pantalla donde se dibujarán los objetos.
-        """
+    def dibuja(self, screen): #el fondo de la imagen
         self.tablero_fondo.dibuja(screen)  # Dibuja el fondo del tablero
         for cuerpo in self.snake_cuerpo:
             cuerpo.dibujar(screen)  # Dibuja cada parte del cuerpo de la serpiente en la pantalla
         self.manzana.dibujar(screen)  # Dibujar la manzana en la pantalla
 
 
-    def actualizar(self):
+    def actualizar(self): #Para actualizar la pantalla
         cabeza_x = self.snake_cuerpo[0].x
         cabeza_y = self.snake_cuerpo[0].y
-
-
-
-
-
-
         print("Cabeza de la serpiente:", cabeza_x, cabeza_y)
         print("Posición de la manzana:", self.manzana.x, self.manzana.y)
         
@@ -61,9 +43,8 @@ class Juego():
             print("¡La serpiente comió la manzana!")
             self.manzana.generar_posicion()  # Generar una nueva posición para la manzana
             # Añadir un nuevo segmento al cuerpo de la serpiente en la posición de la última cola
-            nueva_cola = Cuerpo(self.snake_cuerpo[-1].x, self.snake_cuerpo[-1].y)
+            nueva_cola = Serpiente(self.snake_cuerpo[-1].x, self.snake_cuerpo[-1].y)
             self.snake_cuerpo.append(nueva_cola)
-
         if self.direccion == "UP":
             cabeza_y -= self.velocidad
         elif self.direccion == "DOWN":
@@ -72,7 +53,6 @@ class Juego():
             cabeza_x -= self.velocidad
         elif self.direccion == "RIGHT":
             cabeza_x += self.velocidad
-
         if cabeza_x < 0 or cabeza_x >= cf.SCREEN_WIDTH or cabeza_y < 0 or cabeza_y >= cf.SCREEN_HEIGHT:
             mensaje = f"""¡Colisión! Game Over"""
             font = pygame.font.SysFont(None, 36)
@@ -83,74 +63,35 @@ class Juego():
             pygame.quit()
             sys.exit()
 
-        nueva_cabeza = Cuerpo(cabeza_x, cabeza_y)
+        nueva_cabeza = Serpiente(cabeza_x, cabeza_y)
         self.snake_cuerpo.insert(0, nueva_cabeza)
         self.snake_cuerpo.pop()
 
-
-
-
-class Fondo():
+class Fondo(): #Para que el fondo tenga una imagen
     def __init__(self):
         self.img_fondo = pygame.image.load("escuela.jpg")  # Carga una imagen de fondo para el tablero
         return
-    
-    def dibuja(self, screen):
-        """
-        Dibuja el fondo del tablero en la pantalla.
-        
-        Args:
-            screen (Surface): La superficie de la pantalla donde se dibujará el fondo.
-        """
+    def dibuja(self, screen): 
         rectangulo = self.img_fondo.get_rect()  # Obtiene el rectángulo de la imagen de fondo
         rectangulo.left = 0  # Posiciona el fondo en la esquina superior izquierda
         rectangulo.top = 0
         screen.blit(self.img_fondo, rectangulo)  # Dibuja el fondo en la pantalla
         return
 
-
-class Cuerpo():
+class Serpiente(): #cuerop de la serpiente
     def __init__(self, x, y):
-        """
-        Inicializa un objeto de la clase Cuerpo.
-
-        Args:
-            x (int): La coordenada x inicial del cuerpo.
-            y (int): La coordenada y inicial del cuerpo.
-        """
         self.x = x
         self.y = y
-
     def dibujar(self, screen):
-        """
-        Dibuja el cuerpo en la pantalla.
-
-        Args:
-            screen (Surface): La superficie de la pantalla donde se dibujará el cuerpo.
-        """
-        # El cuerpo
-        # Por simplicidad, vamos a dibujar un rectángulo
         pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(self.x, self.y, 20, 20))
 
 class Manzana():
     def __init__(self):
-        """
-        Inicializa un objeto de la clase Manzana con una posición aleatoria.
-        """
         self.generar_posicion()
 
     def generar_posicion(self):
-        """
-        Genera una posición aleatoria para la manzana dentro del área del tablero.
-        """
         self.x = random.randint(0, cf.SCREEN_WIDTH - 20)  # Posición x aleatoria dentro del ancho del tablero
         self.y = random.randint(0, cf.SCREEN_HEIGHT - 20)  # Posición y aleatoria dentro de la altura del tablero
 
     def dibujar(self, screen):
-        """
-        Dibuja la manzana en la pantalla.
-
-        Args:
-            screen (Surface): La superficie de la pantalla donde se dibujará la manzana.
-        """
         pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(self.x, self.y, 20, 20))
