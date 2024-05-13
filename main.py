84cb3211d844974d6ca21697adba82208449fe0d
FMarin2401 <144305495+FMarin2401@users.noreply.github.com>
2024-05-12 21:14:28 -0600

    quitar comentarios

--- main.py
+++ main.py
@@ -1,32 +1,19 @@
-# Primer programa en Pygame
-import pygame  # Importa el módulo pygame para trabajar con gráficos y sonido
-import config as cf  # Importa el módulo config.py, asumiendo que contiene la clase Config que tiene un atributo deltaT_s
-import snake as sk  # Importa el módulo modulo_juego.py que contiene la clase Juego
-pygame.init()  # Inicializa pygame
-
-
-# Creamos la ventana de juego
-tamano = (cf.SCREEN_WIDTH, cf.SCREEN_HEIGHT)  # Tamaño de la ventana del juego
-screen = pygame.display.set_mode(tamano)  # Crea una ventana con el tamaño especificado
-
-# El tiempo que pasa entre un cuadro y otro
-deltaTiempo_s = cf.Config.deltaT_s  # Obtiene el atributo deltaT_s de la clase Config
-
-# Construir el juego
-snake_juego = sk.Juego()  # Inicializa un objeto de la clase Juego del módulo modulo_juego
-salir = False  # Variable para controlar la salida del bucle principal del juego
-
-# Bucle principal del juego
+import pygame
+import config as cf
+import snake as sk
+pygame.init()
+tamano = (cf.SCREEN_WIDTH, cf.SCREEN_HEIGHT)
+screen = pygame.display.set_mode(tamano)
+deltaTiempo_s = cf.Config.deltaT_s
+snake_juego = sk.Juego()
+salir = False
 while not salir:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            salir = True  # Si se detecta el evento de cerrar la ventana, se establece salir como True
-        snake_juego.movimiento_teclado(event)  # Maneja los eventos de teclado
-    # Actualiza la posición de la serpiente en función de la dirección
+            salir = True
+        snake_juego.movimiento_teclado(event)
     snake_juego.actualizar()
-    # Aquí redibujamos todos los objetos del juego
-    snake_juego.dibuja(screen)  # Llama al método dibuja de la clase Juego para redibujar todos los objetos en la pantalla
-    pygame.display.flip()  # Actualiza la pantalla
-
-pygame.quit()  # Cierra pygame
-print("Fin del programa")  # Imprime un mensaje indicando que el programa ha terminado
+    snake_juego.dibuja(screen)
+    pygame.display.flip()
+pygame.quit()
+print("Fin del programa")
--- snake.py
+++ snake.py
@@ -1,57 +1,47 @@
-
 import pygame
-import config as cf  # Importa el módulo config.py, asumiendo que contiene la clase Config que tiene un atributo deltaT_s
+import config as cf
 import sys
 import random
-tamano = (cf.SCREEN_WIDTH, cf.SCREEN_HEIGHT)  # Tamaño de la ventana del juego
-screen = pygame.display.set_mode(tamano)  # Crea una ventana con el tamaño especificado
+tamano = (cf.SCREEN_WIDTH, cf.SCREEN_HEIGHT)
+screen = pygame.display.set_mode(tamano)
 
 class Juego():
     def __init__(self):
         self.tablero_fondo = Fondo()
-        self.snake_cuerpo = [Serpiente(100, 100)]  # Inicializa la serpiente con un solo segmento en la posición (100, 100)
-        self.direccion = "RIGHT"  # Inicializa la dirección de la serpiente
-        self.velocidad = 0.7 #Velocidad de la serpiente
-        self.manzana = Manzana()  # Agregar la manzana al juego
-        self.segmento_size = 20  # Tamaño inicial de los segmentos de la serpiente
+        self.snake_cuerpo = [Serpiente(100, 100)]
+        self.direccion = "RIGHT"
+        self.velocidad = 0.7
+        self.manzana = Manzana()
+        self.segmento_size = 20
 
-
-    def movimiento_teclado(self, event): #Controles de la serpíente
+    def movimiento_teclado(self, event):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP or event.key == pygame.K_w:
-                if self.direccion != "DOWN":  # Evitar que la serpiente vaya hacia abajo si ya se está moviendo hacia arriba
+                if self.direccion != "DOWN":
                     self.direccion = "UP"
             elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
-                if self.direccion != "UP":  # Evitar que la serpiente vaya hacia arriba si ya se está moviendo hacia abajo
+                if self.direccion != "UP":
                     self.direccion = "DOWN"
             elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                if self.direccion != "RIGHT":  # Evitar que la serpiente vaya hacia la derecha si ya se está moviendo hacia la izquierda
+                if self.direccion != "RIGHT":
                     self.direccion = "LEFT"
             elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                if self.direccion != "LEFT":  # Evitar que la serpiente vaya hacia la izquierda si ya se está moviendo hacia la derecha
+                if self.direccion != "LEFT":
                     self.direccion = "RIGHT"
 
-
-    def dibuja(self, screen): #el fondo de la imagen
-        self.tablero_fondo.dibuja(screen)  # Dibuja el fondo del tablero
+    def dibuja(self, screen):
+        self.tablero_fondo.dibuja(screen)
         for cuerpo in self.snake_cuerpo:
-            cuerpo.dibujar(screen)  # Dibuja cada parte del cuerpo de la serpiente en la pantalla
-        self.manzana.dibujar(screen)  # Dibujar la manzana en la pantalla
+            cuerpo.dibujar(screen)
+        self.manzana.dibujar(screen)
 
-
-    def actualizar(self): #Para actualizar la pantalla
+    def actualizar(self):
         cabeza_x = self.snake_cuerpo[0].x
         cabeza_y = self.snake_cuerpo[0].y
 
-
-        # Verificar si la serpiente come la manzana (con un margen de 10 píxeles alrededor de la posición de la manzana)
-        
         if abs(cabeza_x - self.manzana.x) < 25 and abs(cabeza_y - self.manzana.y) < 25:
-            
-            self.manzana.generar_posicion()  # Generar una nueva posición para la manzana
-            # Aumentar el tamaño de los segmentos de la serpiente
+            self.manzana.generar_posicion()
             self.segmento_size += 900000
-            # Añadir un nuevo segmento al cuerpo de la serpiente en la posición de la última cola
             nueva_cola = Serpiente(self.snake_cuerpo[-1].x, self.snake_cuerpo[-1].y)
             self.snake_cuerpo.append(nueva_cola)
 
@@ -76,18 +66,18 @@ class Juego():
         self.snake_cuerpo.insert(0, nueva_cabeza)
         self.snake_cuerpo.pop()
 
-class Fondo(): #Para que el fondo tenga una imagen
+class Fondo():
     def __init__(self):
-        self.img_fondo = pygame.image.load("escuela.jpg")  # Carga una imagen de fondo para el tablero
+        self.img_fondo = pygame.image.load("escuela.jpg")
         return
-    def dibuja(self, screen): 
-        rectangulo = self.img_fondo.get_rect()  # Obtiene el rectángulo de la imagen de fondo
-        rectangulo.left = 0  # Posiciona el fondo en la esquina superior izquierda
+    def dibuja(self, screen):
+        rectangulo = self.img_fondo.get_rect()
+        rectangulo.left = 0
         rectangulo.top = 0
-        screen.blit(self.img_fondo, rectangulo)  # Dibuja el fondo en la pantalla
+        screen.blit(self.img_fondo, rectangulo)
         return
 
-class Serpiente(): #cuerop de la serpiente
+class Serpiente():
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -99,8 +89,8 @@ class Manzana():
         self.generar_posicion()
 
     def generar_posicion(self):
-        self.x = random.randint(0, cf.SCREEN_WIDTH - 20)  # Posición x aleatoria dentro del ancho del tablero
-        self.y = random.randint(0, cf.SCREEN_HEIGHT - 20)  # Posición y aleatoria dentro de la altura del tablero
+        self.x = random.randint(0, cf.SCREEN_WIDTH - 20)
+        self.y = random.randint(0, cf.SCREEN_HEIGHT - 20)
 
     def dibujar(self, screen):
         pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(self.x, self.y, 25, 25))
